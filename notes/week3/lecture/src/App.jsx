85f43bd1d97cb442e8de988b166e1ccd47d3f38b
Junaid <l1s23bsse0001@ucp.edu.pk>
2025-10-16 09:13:23 +0500

    Remove unused CSS and refactor App component to use Tailwind CSS with new Header, Main, and Footer components

--- notes/week3/lecture/src/App.jsx
+++ notes/week3/lecture/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
-
+import "./style.css";
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
-
             <Header />
             <Main />
             <Footer />
--- notes/week3/lecture/src/components/Header.jsx
+++ notes/week3/lecture/src/components/Header.jsx
@@ -1,14 +1,10 @@
 
 const Header = () => {
-    // inline css
-    const h1 = {
-        fontSize: "25px",
-        color: "red"
-    }
+
 
 
     return (
-        <div style={h1} >Header</div>
+        <div className="ucp" >Header</div>
     )
 }
 
