78aca65256f2126681db3b8c48ee31ecea1e4104
Junaid <l1s23bsse0001@ucp.edu.pk>
2025-10-16 08:48:43 +0500

    Refactor App component structure and add Tailwind CSS support with new Header, Main, and Footer components

---
+++ notes/week3/lecture/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    // inline css
+    const h1 = {
+        fontSize: "25px",
+        color: "red"
+    }
+
+
+    return (
+        <div style={h1} >Header</div>
+    )
+}
+
+export default Header
