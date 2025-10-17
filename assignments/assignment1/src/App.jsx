9716fa8cd10da218e4fbd936a1cea159ae2d054d
Junaid Afzal <junaidafzal2013@gmail.com>
2025-10-17 21:15:22 +0500

    Refactor code structure for improved readability and maintainability

--- assignments/assignment1/src/App.jsx
+++ assignments/assignment1/src/App.jsx
@@ -1,8 +1,16 @@
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Projects from "./components/Projects"
+
 function App() {
 
     return (
         <>
-
+            <Header />
+            <About />
+            <Projects />
+            <Footer />
         </>
     )
 }
