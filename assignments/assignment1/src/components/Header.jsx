0e0667751a052650cc1c86ffcf410eabf3e21b74
Junaid Afzal <junaidafzal2013@gmail.com>
2025-10-18 10:38:42 +0500

    fix: adjust padding in App component and change Header to fixed positioning

--- assignments/assignment1/src/components/Header.jsx
+++ assignments/assignment1/src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
     return (
-        <header className="sticky top-0 z-50 py-4 px-4 sm:px-6 lg:px-8 ">
+        <header className="fixed top-0 left-0 right-0 z-50 py-4 px-4 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto glassmorphic rounded-xl shadow-lg">
                 <div className="flex items-center justify-between p-4">
                     <div className="flex items-center gap-4 cursor-pointer hover:text-primary transition-colors"
