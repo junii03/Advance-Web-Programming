25016d0e08246a578d032e22e2ae218cd2212c5d
Junaid Afzal <junaidafzal2013@gmail.com>
2025-10-24 17:25:25 +0500

    feat: refactor ProjectCard component to use props for better readability and maintainability; add .gitignore for zips directory

--- assignments/assignment1/src/components/ProjectCard.jsx
+++ assignments/assignment1/src/components/ProjectCard.jsx
@@ -1,20 +1,20 @@
-const ProjectCard = ({ image, title, alt, description, link, techStack = [] }) => {
+const ProjectCard = (props) => {
     return (
         <div className="group relative overflow-hidden rounded-xl glassmorphic cursor-pointer transform hover:-translate-y-2 transition-transform duration-300"
 
             onClick={
-                () => { window.open(link, "_blank") }
+                () => { window.open(props.link, "_blank") }
             }>
             <img
                 className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
-                src={image}
-                alt={alt}
+                src={props.image}
+                alt={props.alt}
             />
             <div className="p-6">
-                <h3 className="text-xl font-bold font-display">{title}</h3>
-                <p className="mt-2 text-sm text-gray-400">{description}</p>
+                <h3 className="text-xl font-bold font-display">{props.title}</h3>
+                <p className="mt-2 text-sm text-gray-400">{props.description}</p>
                 <div className="mt-4 flex gap-2">
-                    {techStack.map((tech, index) => (
+                    {props.techStack.map((tech, index) => (
                         <img key={index} src={tech.src} alt={tech.alt} className="w-5 h-5" />
                     ))}
                 </div>
