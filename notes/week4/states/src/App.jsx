d210e9e6e9edbb8b95db796e6b100b1e54ec37f4
Junaid <junaidafdzal2013@gmail.com>
2025-10-29 12:02:42 +0500

    feat: update package-lock.json files to remove peer dependencies; implement form handling in App component with name and email inputs; replace CSS with Tailwind CSS

--- notes/week4/states/src/App.jsx
+++ notes/week4/states/src/App.jsx
@@ -3,15 +3,56 @@ import { useState } from 'react'
 
 
 function App() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Name:", name)
+        console.log("Email:", email)
+        alert(`Form Submitted!\nName: ${name}\nEmail: ${email}`)
+        setName('')
+        setEmail('')
+    }
 
     return (
         <>
-            <h1>Count is {count}</h1>
+            {/* <h1>Count is {count}</h1>
 
             <button type="button" onClick={() => {
                 setCount(count + 1)
-            }}>Increment</button>
+            }}>Increment</button> */}
+
+
+
+            {/* Form Example*/}
+            <form onSubmit={handleSubmit} className='m-4 p-4'>
+                <input
+                    type="text"
+                    onChange={(e) => { setName(e.target.value) }}
+                    placeholder='Name'
+                    value={name}
+                    className='my-4  border-gray border px-1 rounded'
+                />
+
+                <br />
+
+                <input
+                    type="email"
+                    onChange={(e) => { setEmail(e.target.value) }}
+                    placeholder='Email'
+                    value={email}
+                    className='my-4 border-gray border px-1 rounded'
+                />
+
+                <br />
+
+                <button className='my-4 border-gray border rounded px-2' type="submit">Submit</button>
+
+
+            </form>
+
         </>
     )
 }
