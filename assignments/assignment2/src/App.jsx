4188e42669b28453bd841b8a36b021c63f8dc867
Junaid Afzal <junaidafzal2013@gmail.com>
2025-11-08 14:20:52 +0500

    feat: enhance styling by adding theme variables and font classes; update index.html structure

--- assignments/assignment2/src/App.jsx
+++ assignments/assignment2/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <>
             <div className="min-h-screen flex items-center justify-center bg-gray-100">
-                <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS with Vite and React!</h1>
+                <h1 className="text-4xl font-bold text-blue-600 merriweather-regular">Hello, Tailwind CSS with Vite and React!</h1>
             </div>
         </>
     )
